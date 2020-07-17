018f58ed96ea8c07f776e2e8dd25c3d386bc8768
mohammad_ali.eronmicro <mohammad.ali@eronmicro.com>
2020-07-17 12:07:37 +0530

    Updates

---
+++ precache-manifest.004e186ad769798ee18845babc283592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3f06e8e227ed63ac481a1f651f5dce",
+    "url": "/rick-n-morty/index.html"
+  },
+  {
+    "revision": "ec071e31863a905388ad",
+    "url": "/rick-n-morty/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d88f4440e2603f665fe8",
+    "url": "/rick-n-morty/static/js/2.30b9308a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rick-n-morty/static/js/2.30b9308a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec071e31863a905388ad",
+    "url": "/rick-n-morty/static/js/main.9aa0e33e.chunk.js"
+  },
+  {
+    "revision": "0f094abe83481369047f",
+    "url": "/rick-n-morty/static/js/runtime-main.5af91c6d.js"
+  }
+]);
